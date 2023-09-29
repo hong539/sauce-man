79cd56abb259d5ff8e14d760478a7cadcc4e5c24
hong@arch-pyton-dev <yunghunghuang984@gmail.com>
2023-09-30 07:37:07 +0800

    Test codes

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from typing import Optional
 import discord
 from discord import app_commands
 import yaml
@@ -19,6 +20,7 @@ def load_config(path: str) -> dict:
 
 
 class MyClient(discord.Client):
+        
     def __init__(self) -> None:
         # Just default intents and a `discord.Client` instance
         # We don't need a `commands.Bot` instance because we are not
@@ -29,7 +31,7 @@ class MyClient(discord.Client):
 
         # We need an `discord.app_commands.CommandTree` instance
         # to register application commands (slash commands in this case)
-        self.tree = app_commands.CommandTree(self)
+        self.tree = app_commands.CommandTree(self)        
     
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
@@ -39,10 +41,86 @@ class MyClient(discord.Client):
         print(f'Message from {message.channel}:{message.author}: {message.content}')
 
 def main():
+    
     BASE_DIR = Path(__file__).resolve().parent
     config_path = os.path.join(BASE_DIR, 'my_self.yaml')
-    temp = load_config(config_path)
+    temp = load_config(config_path)    
     client = MyClient()
+    
+    @client.tree.command()
+    async def hello(interaction: discord.Interaction):
+        """Says hello!"""
+        await interaction.response.send_message(f'Hi, {interaction.user.mention}')
+
+    @client.tree.command()
+    @app_commands.describe(
+        first_value='The first value you want to add something to',
+        second_value='The value you want to add to the first value',
+    )
+    async def add(interaction: discord.Interaction, first_value: int, second_value: int):
+        """Adds two numbers together."""
+        await interaction.response.send_message(f'{first_value} + {second_value} = {first_value + second_value}')
+
+
+    # The rename decorator allows us to change the display of the parameter on Discord.
+    # In this example, even though we use `text_to_send` in the code, the client will use `text` instead.
+    # Note that other decorators will still refer to it as `text_to_send` in the code.
+    @client.tree.command()
+    @app_commands.rename(text_to_send='text')
+    @app_commands.describe(text_to_send='Text to send in the current channel')
+    async def send(interaction: discord.Interaction, text_to_send: str):
+        """Sends the text into the current channel."""
+        await interaction.response.send_message(text_to_send)
+
+
+    # To make an argument optional, you can either give it a supported default argument
+    # or you can mark it as Optional from the typing standard library. This example does both.
+    @client.tree.command()
+    @app_commands.describe(member='The member you want to get the joined date from; defaults to the user who uses the command')
+    async def joined(interaction: discord.Interaction, member: Optional[discord.Member] = None):
+        """Says when a member joined."""
+        # If no member is explicitly provided then we use the command user here
+        member = member or interaction.user
+
+        # The format_dt function formats the date time into a human readable representation in the official client
+        await interaction.response.send_message(f'{member} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+    # A Context Menu command is an app command that can be run on a member or on a message by
+    # accessing a menu within the client, usually via right clicking.
+    # It always takes an interaction as its first parameter and a Member or Message as its second parameter.
+
+    # This context menu command only works on members
+    @client.tree.context_menu(name='Show Join Date')
+    async def show_join_date(interaction: discord.Interaction, member: discord.Member):
+        # The format_dt function formats the date time into a human readable representation in the official client
+        await interaction.response.send_message(f'{member} joined at {discord.utils.format_dt(member.joined_at)}')
+
+
+    # This context menu command only works on messages
+    @client.tree.context_menu(name='Report to Moderators')
+    async def report_message(interaction: discord.Interaction, message: discord.Message):
+        # We're sending this response message with ephemeral=True, so only the command executor can see it
+        await interaction.response.send_message(
+            f'Thanks for reporting this message by {message.author.mention} to our moderators.', ephemeral=True
+        )
+
+        # Handle report by sending it into a log channel
+        log_channel = interaction.guild.get_channel(0)  # replace with your channel id
+
+        embed = discord.Embed(title='Reported Message')
+        if message.content:
+            embed.description = message.content
+
+        embed.set_author(name=message.author.display_name, icon_url=message.author.display_avatar.url)
+        embed.timestamp = message.created_at
+
+        url_view = discord.ui.View()
+        url_view.add_item(discord.ui.Button(label='Go to Message', style=discord.ButtonStyle.url, url=message.jump_url))
+
+        await log_channel.send(embed=embed, view=url_view)
+
+    
     client.run(token= temp["bot"]["token"])    
 
 if __name__ == "__main__":
