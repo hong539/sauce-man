9ad7a0a9e5a19ee1b0bb9e60b80d3da9ecd31a1b
hong@arch-pyton-dev <yunghunghuang984@gmail.com>
2023-09-30 04:34:10 +0800

    Test codes

--- src/main.py
+++ src/main.py
@@ -1,6 +1,22 @@
+import os
+from pathlib import Path
 import discord
 from discord import app_commands
 import yaml
+
+def load_config(path: str) -> dict:
+    """Load configuration data from a YAML file.
+
+    Args:
+        path (str): The path to the YAML configuration file.
+    """
+    with open(path, "r") as config:
+        config_data = yaml.safe_load(config)
+        print("config_data type is:", type(config_data))
+        print("bot token is:\n", config_data["bot"]["token"])
+        print("discord guild ID is:\n", config_data["bot"]["guild_id"])
+    return config_data
+
 
 class MyClient(discord.Client):
     def __init__(self) -> None:
@@ -14,24 +30,6 @@ class MyClient(discord.Client):
         # We need an `discord.app_commands.CommandTree` instance
         # to register application commands (slash commands in this case)
         self.tree = app_commands.CommandTree(self)
-
-    def load_config(self, path):
-        """Load configuration data from a YAML file.
-
-        Args:
-            path (str): The path to the YAML configuration file.
-        """
-        with open(path, "r") as config:
-            self.config_data = yaml.safe_load(config)
-            print("config_data type is:", type(self.config_data))
-            print("bot token is:\n", self.config_data["bot"]["token"])
-            print("discord guild ID is:\n", self.config_data["bot"]["guild_id"])
-        return self.config_data
-
-    async def setup_hook(self):
-        # This copies the global commands over to your guild.
-        self.tree.copy_global_to(guild=MY_GUILD)
-        await self.tree.sync(guild=MY_GUILD)
     
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
@@ -39,12 +37,13 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         print(f'Message from {message.channel}:{message.author}: {message.content}')
-        
-        if message.author == client.user:
-            return
-        if message.content.startswith('$hello'):
-            await message.channel.send('Hello!')
+
+def main():
+    BASE_DIR = Path(__file__).resolve().parent
+    config_path = os.path.join(BASE_DIR, 'my_self.yaml')
+    temp = load_config(config_path)
+    client = MyClient()
+    client.run(token= temp["bot"]["token"])    
 
 if __name__ == "__main__":
-    client = MyClient()
-    client.run(token= client.load_config("./my_self.yaml")["bot"]["token"])
+    main()
